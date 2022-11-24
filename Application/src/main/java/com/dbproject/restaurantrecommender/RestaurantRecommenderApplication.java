86101236a29da7d5b4a60779797840a8b864a62b
Kanika Dhiman <kanikadhiman@Kanikas-MacBook-Air.local>
2022-11-23 19:53:58 -0500

    Initial Setup

---
+++ Application/src/main/java/com/dbproject/restaurantrecommender/RestaurantRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.dbproject.restaurantrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantRecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantRecommenderApplication.class, args);
+    }
+
+}
