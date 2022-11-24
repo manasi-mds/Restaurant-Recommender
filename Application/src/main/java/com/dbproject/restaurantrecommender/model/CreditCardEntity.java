f8a91bb072b434c0d8dc0a1e4402c2c221108624
Kanika Dhiman <kanikadhiman@Kanikas-MacBook-Air.local>
2022-11-24 14:54:08 -0500

    Missing lombok

--- Application/src/main/java/com/dbproject/restaurantrecommender/model/CreditCardEntity.java
+++ Application/src/main/java/com/dbproject/restaurantrecommender/model/CreditCardEntity.java
@@ -1,8 +1,10 @@
 package com.dbproject.restaurantrecommender.model;
 
+import lombok.Data;
 import org.springframework.data.neo4j.core.schema.Node;
 
 @Node("Credit_Card")
+@Data
 public class CreditCardEntity extends BaseEntity{
     String name;
 
--- Application/src/main/java/com/dbproject/restaurantrecommender/model/PersonEntity.java
+++ Application/src/main/java/com/dbproject/restaurantrecommender/model/PersonEntity.java
@@ -1,8 +1,10 @@
 package com.dbproject.restaurantrecommender.model;
 
+import lombok.Data;
 import org.springframework.data.neo4j.core.schema.Node;
 
 @Node("Person")
+@Data
 public class PersonEntity extends BaseEntity{
     String name;
     String email;
