b8421233262d59aa827fd693178dae1100abfb56
Kanika Dhiman <kanikadhiman@lawn-128-61-123-79.lawn.gatech.edu>
2022-12-02 14:42:56 -0500

    Refactor

--- Application/src/main/java/com/dbproject/restaurantrecommender/services/UserService.java
+++ Application/src/main/java/com/dbproject/restaurantrecommender/services/UserService.java
@@ -5,7 +5,6 @@ import com.dbproject.restaurantrecommender.dto.UserDTO;
 import com.dbproject.restaurantrecommender.dto.UserPreferenceDTO;
 import com.dbproject.restaurantrecommender.dto.preference.AmbiencePreferenceDTO;
 import com.dbproject.restaurantrecommender.dto.preference.CuisinePreferenceDTO;
-import com.dbproject.restaurantrecommender.dto.preference.OutdoorSeatingPreferenceDTO;
 import com.dbproject.restaurantrecommender.enums.WifiType;
 import com.dbproject.restaurantrecommender.mapper.RestaurantMapper;
 import com.dbproject.restaurantrecommender.mapper.UserMapper;
@@ -95,21 +94,57 @@ public class UserService implements IUserService {
     @Override
     public void createPreference(Long userId, UserPreferenceDTO userPreferenceDTO) {
         UserEntity user = verifyUser(userId);
-        List<CuisineEntity> cuisines;
-        if (userPreferenceDTO.getCuisines() != null) {
-            Map<Long, Integer> cuisineIdWithWeights = userPreferenceDTO.getCuisines().stream().collect(Collectors.toMap(CuisinePreferenceDTO::getCuisineId, CuisinePreferenceDTO::getWeight));
-            cuisines = cuisineRepository.findAllById(userPreferenceDTO.getCuisines().stream().map(CuisinePreferenceDTO::getCuisineId).collect(Collectors.toList()));
-            Map<CuisineEntity, Integer> preferredCuisineWeight = cuisines.stream().collect(Collectors.toMap(cuisine -> cuisine, cuisine -> cuisineIdWithWeights.get(cuisine.getId())));
-            user.addCuisinePreferences(preferredCuisineWeight);
-        }
-        List<AmbienceEntity> ambiences;
-        if (userPreferenceDTO.getAmbiences() != null) {
-            Map<Long, Integer> ambienceIdWithWeights = userPreferenceDTO.getAmbiences().stream().collect(Collectors.toMap(AmbiencePreferenceDTO::getAmbienceId, AmbiencePreferenceDTO::getWeight));
-            ambiences = ambienceRepository.findAllById(userPreferenceDTO.getAmbiences().stream().map(AmbiencePreferenceDTO::getAmbienceId).collect(Collectors.toList()));
-            Map<AmbienceEntity, Integer> preferredAmbienceWeight = ambiences.stream().collect(Collectors.toMap(ambience -> ambience, ambience -> ambienceIdWithWeights.get(ambience.getId())));
-            user.addAmbiencePreferences(preferredAmbienceWeight);
-        }
+        addCuisinePreference(userPreferenceDTO, user);
+        addAmbiencePreference(userPreferenceDTO, user);
+        addWifiPreference(userPreferenceDTO, user);
+        addAlcoholPreference(userPreferenceDTO, user);
+        addCreditCardPreference(userPreferenceDTO, user);
+        addOutdoorSeatingPreference(userPreferenceDTO, user);
+        addRatingPreference(userPreferenceDTO, user);
+        userRepository.save(user);
+    }
 
+    private void addRatingPreference(UserPreferenceDTO userPreferenceDTO, UserEntity user) {
+        if(userPreferenceDTO.getMinimumRating()!=null && userPreferenceDTO.getMinimumRating().getMinRating() != null) {
+            checkRating(userPreferenceDTO.getMinimumRating().getMinRating());
+            Optional<RatingEntity> ratingEntity = ratingRepository.findByName(userPreferenceDTO.getMinimumRating().getMinRating().toString());
+            Preconditions.checkArgument(ratingEntity.isPresent(), "Rating info " + userPreferenceDTO.getMinimumRating().getMinRating() + " does not exist in the database");
+            user.addRatingPreference(ratingEntity.get(), userPreferenceDTO.getMinimumRating().getWeight());
+        }
+    }
+
+    private void addOutdoorSeatingPreference(UserPreferenceDTO userPreferenceDTO, UserEntity user) {
+        if(userPreferenceDTO.getOutdoorSeating()!=null && userPreferenceDTO.getOutdoorSeating().getIsOutdoorSeatingAvailable() != null) {
+            String outdoorSeating = userPreferenceDTO.getOutdoorSeating().getIsOutdoorSeatingAvailable() ? "true" : null;
+            if(outdoorSeating!=null) {
+                Optional<OutdoorSeatingEntity> outdoorSeatingEntity = outdoorSeatingRepository.findByName(outdoorSeating);
+                Preconditions.checkArgument(outdoorSeatingEntity.isPresent(), "Outdoor seating info " + outdoorSeating + " does not exist in the database");
+                user.addOutdoorSeatingPreference(outdoorSeatingEntity.get(), userPreferenceDTO.getOutdoorSeating().getWeight());
+            }
+        }
+    }
+
+    private void addCreditCardPreference(UserPreferenceDTO userPreferenceDTO, UserEntity user) {
+        if(userPreferenceDTO.getCreditCardAccepted()!= null && userPreferenceDTO.getCreditCardAccepted().getIsCreditCardAccepted() != null) {
+            String creditCardAccepted = userPreferenceDTO.getCreditCardAccepted().getIsCreditCardAccepted() ? "yes" : null;
+            if(creditCardAccepted!=null) {
+                Optional<CreditCardEntity> creditCardEntity = creditCardRepository.findByName(creditCardAccepted);
+                Preconditions.checkArgument(creditCardEntity.isPresent(), "Credit card accepted info " + creditCardAccepted + " does not exist in the database");
+                user.addCreditCardPreference(creditCardEntity.get(), userPreferenceDTO.getCreditCardAccepted().getWeight());
+            }
+        }
+    }
+
+    private void addAlcoholPreference(UserPreferenceDTO userPreferenceDTO, UserEntity user) {
+        if (userPreferenceDTO.getAlcoholServed() != null && userPreferenceDTO.getAlcoholServed().getIsAlcoholServed() != null) {
+            String alcoholServed = userPreferenceDTO.getAlcoholServed().getIsAlcoholServed() ? "yes" : "no";
+            Optional<AlcoholEntity> alcoholEntity = alcoholRepository.findByName(alcoholServed);
+            Preconditions.checkArgument(alcoholEntity.isPresent(), "Alcohol served info " + alcoholServed + " does not exist in the database");
+            user.addAlcoholPreference(alcoholEntity.get(), userPreferenceDTO.getAlcoholServed().getWeight());
+        }
+    }
+
+    private void addWifiPreference(UserPreferenceDTO userPreferenceDTO, UserEntity user) {
         if (userPreferenceDTO.getWifiTypeAvailable() != null && StringUtils.isNotBlank((userPreferenceDTO.getWifiTypeAvailable().getWifiType()))) {
             WifiType type = switch (userPreferenceDTO.getWifiTypeAvailable().getWifiType().toLowerCase()) {
                 case "free" -> WifiType.free;
@@ -120,40 +155,26 @@ public class UserService implements IUserService {
             Preconditions.checkArgument(wifiEntity.isPresent(), "Wifi type " + type + " does not exist in the database");
             user.addWifiPreference(wifiEntity.get(), userPreferenceDTO.getWifiTypeAvailable().getWeight());
         }
+    }
 
-        if (userPreferenceDTO.getAlcoholServed() != null && userPreferenceDTO.getAlcoholServed().getIsAlcoholServed() != null) {
-            String alcoholServed = userPreferenceDTO.getAlcoholServed().getIsAlcoholServed() ? "yes" : "no";
-            Optional<AlcoholEntity> alcoholEntity = alcoholRepository.findByName(alcoholServed);
-            Preconditions.checkArgument(alcoholEntity.isPresent(), "Alcohol served info " + alcoholServed + " does not exist in the database");
-            user.addAlcoholPreference(alcoholEntity.get(), userPreferenceDTO.getAlcoholServed().getWeight());
+    private void addAmbiencePreference(UserPreferenceDTO userPreferenceDTO, UserEntity user) {
+        List<AmbienceEntity> ambiences;
+        if (userPreferenceDTO.getAmbiences() != null) {
+            Map<Long, Integer> ambienceIdWithWeights = userPreferenceDTO.getAmbiences().stream().collect(Collectors.toMap(AmbiencePreferenceDTO::getAmbienceId, AmbiencePreferenceDTO::getWeight));
+            ambiences = ambienceRepository.findAllById(userPreferenceDTO.getAmbiences().stream().map(AmbiencePreferenceDTO::getAmbienceId).collect(Collectors.toList()));
+            Map<AmbienceEntity, Integer> preferredAmbienceWeight = ambiences.stream().collect(Collectors.toMap(ambience -> ambience, ambience -> ambienceIdWithWeights.get(ambience.getId())));
+            user.addAmbiencePreferences(preferredAmbienceWeight);
         }
+    }
 
-        if(userPreferenceDTO.getCreditCardAccepted()!= null && userPreferenceDTO.getCreditCardAccepted().getIsCreditCardAccepted() != null) {
-            String creditCardAccepted = userPreferenceDTO.getCreditCardAccepted().getIsCreditCardAccepted() ? "yes" : null;
-            if(creditCardAccepted!=null) {
-                Optional<CreditCardEntity> creditCardEntity = creditCardRepository.findByName(creditCardAccepted);
-                Preconditions.checkArgument(creditCardEntity.isPresent(), "Credit card accepted info " + creditCardAccepted + " does not exist in the database");
-                user.addCreditCardPreference(creditCardEntity.get(), userPreferenceDTO.getCreditCardAccepted().getWeight());
-            }
+    private void addCuisinePreference(UserPreferenceDTO userPreferenceDTO, UserEntity user) {
+        List<CuisineEntity> cuisines;
+        if (userPreferenceDTO.getCuisines() != null) {
+            Map<Long, Integer> cuisineIdWithWeights = userPreferenceDTO.getCuisines().stream().collect(Collectors.toMap(CuisinePreferenceDTO::getCuisineId, CuisinePreferenceDTO::getWeight));
+            cuisines = cuisineRepository.findAllById(userPreferenceDTO.getCuisines().stream().map(CuisinePreferenceDTO::getCuisineId).collect(Collectors.toList()));
+            Map<CuisineEntity, Integer> preferredCuisineWeight = cuisines.stream().collect(Collectors.toMap(cuisine -> cuisine, cuisine -> cuisineIdWithWeights.get(cuisine.getId())));
+            user.addCuisinePreferences(preferredCuisineWeight);
         }
-
-        if(userPreferenceDTO.getOutdoorSeating()!=null && userPreferenceDTO.getOutdoorSeating().getIsOutdoorSeatingAvailable() != null) {
-            String outdoorSeating = userPreferenceDTO.getOutdoorSeating().getIsOutdoorSeatingAvailable() ? "true" : null;
-            if(outdoorSeating!=null) {
-                Optional<OutdoorSeatingEntity> outdoorSeatingEntity = outdoorSeatingRepository.findByName(outdoorSeating);
-                Preconditions.checkArgument(outdoorSeatingEntity.isPresent(), "Outdoor seating info " + outdoorSeating + " does not exist in the database");
-                user.addOutdoorSeatingPreference(outdoorSeatingEntity.get(), userPreferenceDTO.getOutdoorSeating().getWeight());
-            }
-        }
-
-        if(userPreferenceDTO.getMinimumRating()!=null && userPreferenceDTO.getMinimumRating().getMinRating() != null) {
-            checkRating(userPreferenceDTO.getMinimumRating().getMinRating());
-            Optional<RatingEntity> ratingEntity = ratingRepository.findByName(userPreferenceDTO.getMinimumRating().getMinRating().toString());
-            Preconditions.checkArgument(ratingEntity.isPresent(), "Rating info " + userPreferenceDTO.getMinimumRating().getMinRating() + " does not exist in the database");
-            user.addRatingPreference(ratingEntity.get(), userPreferenceDTO.getMinimumRating().getWeight());
-        }
-
-        userRepository.save(user);
     }
 
     private void checkRating(Float minRating) {
