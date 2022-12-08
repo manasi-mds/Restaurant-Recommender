912161f7f66694fd274b7571ce43ef1875ff1747
Kanika Dhiman <kanikadhiman@lawn-128-61-119-156.lawn.gatech.edu>
2022-12-07 22:28:18 -0500

    User Preference GET API

--- Application/src/main/java/com/dbproject/restaurantrecommender/dto/preference/AmbiencePreferenceDTO.java
+++ Application/src/main/java/com/dbproject/restaurantrecommender/dto/preference/AmbiencePreferenceDTO.java
@@ -1,9 +1,14 @@
 package com.dbproject.restaurantrecommender.dto.preference;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AmbiencePreferenceDTO {
     Long ambienceId;
+    String ambienceName;
     Integer weight;
 }
--- Application/src/main/java/com/dbproject/restaurantrecommender/mapper/UserPreferenceMapper.java
+++ Application/src/main/java/com/dbproject/restaurantrecommender/mapper/UserPreferenceMapper.java
@@ -1,8 +1,9 @@
 package com.dbproject.restaurantrecommender.mapper;
 
 import com.dbproject.restaurantrecommender.dto.UserPreferenceDTO;
-import com.dbproject.restaurantrecommender.dto.preference.AlcoholServedPreferenceDTO;
-import com.dbproject.restaurantrecommender.dto.preference.CuisinePreferenceDTO;
+import com.dbproject.restaurantrecommender.dto.preference.*;
+import com.dbproject.restaurantrecommender.model.AmbiencePreference;
+import com.dbproject.restaurantrecommender.model.CuisinePreference;
 import com.dbproject.restaurantrecommender.model.UserEntity;
 
 
@@ -23,12 +24,47 @@ public class UserPreferenceMapper {
         userDTO.setPassword(userEntity.getPassword());
 
         AlcoholServedPreferenceDTO alcoholServedPreferenceDTO = new AlcoholServedPreferenceDTO();
-        if(userEntity.getAlcoholPreference()!=null) alcoholServedPreferenceDTO.setIsAlcoholServed(userEntity.getAlcoholPreference().getAlcoholEntity().isAlcoholServed());
-//        userDTO.setCuisines(userEntity.getCuisinePreferences().stream().map(cuisinePreferenceEntity -> {
-////            return new CuisinePreferenceDTO(cuisinePreferenceEntity.getCuisineEntity());
-//        }).toList());
+        if(userEntity.getAlcoholPreference()!=null)
+            alcoholServedPreferenceDTO.setIsAlcoholServed(userEntity.getAlcoholPreference().getAlcoholEntity().isAlcoholServed());
+        userDTO.setAlcoholServed(alcoholServedPreferenceDTO);
 
+        for(CuisinePreference up:userEntity.getCuisinePreferences()){
+            CuisinePreferenceDTO cuisinePreferenceDTO = new CuisinePreferenceDTO();
+            cuisinePreferenceDTO.setCuisineId(up.getCuisineEntity().getId());
+            cuisinePreferenceDTO.setCuisineName(up.getCuisineEntity().getName());
+            cuisinePreferenceDTO.setWeight(up.getWeight());
+            userDTO.getCuisines().add(cuisinePreferenceDTO);
+        }
 
+        for(AmbiencePreference ap : userEntity.getAmbiencePreferences()){
+            AmbiencePreferenceDTO ambiencePreferenceDTO = new AmbiencePreferenceDTO();
+            ambiencePreferenceDTO.setAmbienceId(ap.getAmbienceEntity().getId());
+            ambiencePreferenceDTO.setAmbienceName(ap.getAmbienceEntity().getName());
+            ambiencePreferenceDTO.setWeight(ap.getWeight());
+            userDTO.getAmbiences().add(ambiencePreferenceDTO);
+        }
+
+        CreditCardPreferenceDTO creditCardPreferenceDTO = new CreditCardPreferenceDTO();
+        if(userEntity.getCreditCardPreference()!=null)
+            creditCardPreferenceDTO.setIsCreditCardAccepted(userEntity.getCreditCardPreference().getCreditCardEntity().isCreditCardAccepted());
+        userDTO.setCreditCardAccepted(creditCardPreferenceDTO);
+
+        WifiPreferenceDTO wifiPreferenceDTO = new WifiPreferenceDTO();
+        if(userEntity.getWifiPreference()!=null)
+            wifiPreferenceDTO.setWifiType(userEntity.getWifiPreference().getWifi().getType().toString());
+        userDTO.setWifiTypeAvailable(wifiPreferenceDTO);
+
+        RatingPreferenceDTO ratingPreferenceDTO = new RatingPreferenceDTO();
+        if(userEntity.getMinimumRating()!=null)
+            ratingPreferenceDTO.setMinRating(userEntity.getMinimumRating().getRatingEntity().getRating());
+        userDTO.setMinimumRating(ratingPreferenceDTO);
+
+        OutdoorSeatingPreferenceDTO outdoorSeatingPreferenceDTO = new OutdoorSeatingPreferenceDTO();
+        if(userEntity.getOutdoorSeatingPreference()!=null)
+            outdoorSeatingPreferenceDTO.setIsOutdoorSeatingAvailable(userEntity.getOutdoorSeatingPreference().getOutdoorSeatingEntity().isOutdoorSeatingAvailable());
+        userDTO.setOutdoorSeating(outdoorSeatingPreferenceDTO);
+
+        userDTO.setDistance(userEntity.getDistancePreference());
         return userDTO;
     }
 }
